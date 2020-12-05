8e508869f1487c07ab8c8cd81290b6bd71127b61
178900PRITAM <61016345+178900PRITAM@users.noreply.github.com>
2020-12-05 15:04:33 +0530

    Welcome

--- views/welcome.php
+++ views/welcome.php
@@ -5,5 +5,5 @@
 	But there was a change borught by Prin Framework<br>
 	chacnge was<br>
 	You dont have to create new mysqliconnect<br>
-	mysqli connect var is  $connect<br>
+	mysqli connect var is  $mysql<br>
 	just enabe in config.php USE_DB = true<br>
