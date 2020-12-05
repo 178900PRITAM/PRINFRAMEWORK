8f85078dd9c814523aad6f2654b2c5b6a39a3970
178900PRITAM <61016345+178900PRITAM@users.noreply.github.com>
2020-12-05 14:51:25 +0530

    Update welcome.php

--- views/welcome.php
+++ views/welcome.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Welcome to Prin Framework</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-</head>
-<body>
-<div class="card m-5">
+
 	Requirments : <b>PHP 5.6+</b><br>
 <br>
 	Your Bascic php code<br>
@@ -16,6 +7,3 @@
 	You dont have to create new mysqliconnect<br>
 	mysqli connect var is  $connect<br>
 	just enabe in config.php USE_DB = true<br>
-</div>
-</body>
-</html>
