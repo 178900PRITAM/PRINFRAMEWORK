3383b2198847776c75b6704d2e3575821f064089
178900PRITAM <61016345+178900PRITAM@users.noreply.github.com>
2020-12-05 14:19:41 +0530

    Create prinCORE.php

---
+++ core/prinCORE.php
@@ -0,0 +1,53 @@
+<?php
+if (file_exists("data.php")) {
+	include 'data.php';
+}else{
+	die("Error : Something went wrong");
+}
+if (file_exists("config.php")) {
+	include 'config.php';
+}else{
+	die("Error : Something went wrong");
+}
+if ($load['env'] == 'developement') {
+function HandleError($errno, $errstr, $errfile, $errline) {
+    echo "<b>Error:</b> Backtrace : [$errno] $errstr<br>";
+    echo " Error on line no. $errline in File :  $errfile<br>";
+}
+set_error_handler("HandleError");
+}
+function getControllerStatus($controllerarray){
+	$controller = $controllerarray;
+	if (isset($controller['name'])) {
+		$status = 200 ;
+	}else{
+		$status = 404 ;
+	}
+	return $status;
+}
+$ht = "RewriteEngine On\nRewriteRule ^([".$allowed_char."]+)$ index.php?page=$1
+";
+if (file_exists(".htaccess")) {
+	$gethtaccess = fopen(".htaccess", "r") or die("WENT WRONG");
+	if ($gethtaccess == $ht) {
+	}else{
+		$htaccess = fopen(".htaccess", "w") or die("htaccess Error!");
+		fwrite($htaccess, $ht);
+		fclose($htaccess);
+	}
+}else{
+$htaccess = fopen(".htaccess", "w") or die("htaccess Error!");
+fwrite($htaccess, $ht);
+fclose($htaccess);
+}
+function loadfile($file,$ext){
+	#file
+	if (file_exists("views/".$file.".".$ext."")) {
+		include "views/".$file.".".$ext."";
+	}else{
+		echo "<script>alert('Error View File ".$file."".$ext."');</script>";
+	}
+}
+function redirect($url){
+	echo '<script> document.location.href="'.$url.'"; </script>';
+}
