dc987c42745f96e5da01f2c38c62dc0d69b1b77f
178900PRITAM <61016345+178900PRITAM@users.noreply.github.com>
2020-12-05 14:50:25 +0530

    Add files via upload

---
+++ system/404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error 404 : <?php echo $_GET['page']; ?></title>
+</head>
+<body>
+<style>
+	body{
+		background: gray;
+	}
+	.error{
+		border: 1px solid red;
+		color: white;
+		margin:3%;
+	}
+	.error h5{
+		margin: 1%;
+	}
+</style>
+<div class="error">
+	<h5>Error Page <?=$_GET['page']?> Not Found</h5>
+</div>
+</body>
+</html>
