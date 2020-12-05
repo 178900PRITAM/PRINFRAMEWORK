6870f65ba08dd54e4b459f037a81861d6c0315bd
178900PRITAM <61016345+178900PRITAM@users.noreply.github.com>
2020-12-05 16:46:38 +0530

    Added CORE,VIEWS,SYSTEM Security

---
+++ system/403.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Folder Forbiddened</title>
+</head>
+<body>
+<style>
+	body{
+		background: #b5d1ff;
+	}
+	.error{
+		border: 1px solid red;
+		color: red;
+		margin:3%;
+	}
+	.error h5{
+		margin: 1%;
+	}
+</style>
+<div class="error">
+	<h5><error>Something went wrong</error></h5>
+</div>
+</body>
+</html>
